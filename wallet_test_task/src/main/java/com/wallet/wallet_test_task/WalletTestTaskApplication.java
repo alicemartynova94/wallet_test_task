0ebc811051ff4a2e25b2fb67c477104b02ec35e0
Alice Martynova <alicemartynova94@gmail.com>
2024-09-27 14:45:26 +0300

    initial commit

---
+++ wallet_test_task/src/main/java/com/wallet/wallet_test_task/WalletTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.wallet_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletTestTaskApplication.class, args);
+	}
+
+}
---
+++ wallet_test_task/src/test/java/com/wallet/wallet_test_task/WalletTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.wallet_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
